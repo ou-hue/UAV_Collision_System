7f63c0343dc6f3c3cf8706cccfd5e23c2ebc688e
OUMAIMA_BOURAADA <bouraadaoumaima1@gmail.com>
2026-05-08 23:03:04 +0100

    Add comments and update function parameter names

--- collision_system.h
+++ collision_system.h
@@ -1,16 +1,24 @@
 #ifndef COLLISION_SYSTEM_H
 #define COLLISION_SYSTEM_H
 
+// structure d'un drone
 typedef struct {
-    int id;
-    float x;
-    float y;
-    float z;
+    int id;      // numéro unique du drone
+    float x;     // coordonnée X
+    float y;     // coordonnée Y
+    float z;     // altitude
 } Drone;
 
+// alloue la mémoire pour N drones (un seul bloc contigu)
 Drone* create_swarm(int N);
+
+// initialise les positions avec des valeurs aléatoires
 void init_swarm(Drone* swarm, int N);
-void detect_collisions(Drone* swarm, int N, float threshold);
+
+// détecte toutes les collisions (tri par X + fenêtre glissante)
+void detect_collisions(Drone* swarm, int N, float seuil);
+
+// libère la mémoire
 void free_swarm(Drone* swarm);
 
 #endif
